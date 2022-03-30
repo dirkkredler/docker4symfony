3a70bab11b1dc3bdc673109bebe53697546d04a6
Dirk Kredler <kredler@shk-nrw.de>
2022-03-30 13:14:37 +0200

    Add api controller for songs

---
+++ src/Controller/SongController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1); 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route("/api/songs/{id}")]
+final class SongController
+{
+    public function __invoke(int $id): JsonResponse
+    {
+        $songs = [
+            $id => [
+                'id' => $id,
+                'name' => 'Waterfalls',
+                'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+            ],
+        ];
+
+        return new JsonResponse($songs[$id]);
+    }
+}
