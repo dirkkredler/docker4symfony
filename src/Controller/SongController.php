de9d1fc1dc58c0a7ebdc5756a8543d53a359e6f3
Dirk Kredler <kredler@shk-nrw.de>
2022-04-04 10:01:57 +0200

    Add logging

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route("/api/songs/{id<\d+>}", methods: ["GET"])]
 final class SongController
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function __invoke(int $id): JsonResponse
     {
         $songs = [
@@ -19,6 +24,12 @@ final class SongController
                 'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
             ],
         ];
+
+        $this->logger->info("Song id: {$id}");
+        $this->logger->info("Song id: {id}", [
+            'id' => $id
+        ]);
+
 
         return new JsonResponse($songs[$id]);
     }
